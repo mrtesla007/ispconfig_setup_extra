e3b4efe9f2a1be3f7098f68021a4b0f6ddd7e968
BigLorD <FuryTr@yandex.com>
2018-12-27 12:06:54 +0300

    Create ioncube.sh

---
+++ ioncube.sh
@@ -0,0 +1,11 @@
+  echo -n "Installing Ioncube Loader (PHP 7)... "
+  mkdir /usr/local/ioncube
+  wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+  tar xzvf ioncube_loaders_lin_x86-64.tar.gz
+  cd ioncube
+  cp -p ioncube_loader_lin_7.3.so /usr/local/ioncube
+  cp -p ioncube_loader_lin_7.3_ts.so /usr/local/ioncube
+	sed -i "s/;zend_extension=zend_extension=/usr/local/ioncube/ioncube_loader_lin_7.3.so" /etc/php.ini
+  sed -i "s/;zend_extension_ts=zend_extension_ts=/usr/local/ioncube/ioncube_loader_lin_7.3_ts.so" /etc/php.ini
+  service php-fpm restart
+  echo -e "[${green}DONE${NC}]\n"
