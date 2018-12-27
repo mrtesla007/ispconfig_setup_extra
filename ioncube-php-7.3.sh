f060bb5f7d54750db49ab337c75f10f7fe617e44
BigLorD <FuryTr@yandex.com>
2018-12-27 13:22:41 +0300

    Update ioncube-php-7.3.sh

--- ioncube-php-7.3.sh
+++ ioncube-php-7.3.sh
@@ -5,7 +5,13 @@
   cd ioncube
   cp -p ioncube_loader_lin_7.3.so /usr/local/ioncube
   cp -p ioncube_loader_lin_7.3_ts.so /usr/local/ioncube
-sed -i "s/;zend_extension=/zend_extension=/usr/local/ioncube/ioncube_loader_lin_7.3.so" /etc/php.ini
-sed -i "s/;zend_extension_ts=/zend_extension_ts=/usr/local/ioncube/ioncube_loader_lin_7.3_ts.so" /etc/php.ini
+  
+#sed -i "s/;zend_extension=/zend_extension=/usr/local/ioncube/ioncube_loader_lin_7.3.so" /etc/php.ini
+#sed -i "s%;zend_extension_ts=%zend_extension_ts=/usr/local/ioncube/ioncube_loader_lin_7.3_ts.so" /etc/php.ini
+
+  #normalde ustteki komutlar ile calismak lazim ama tam kavrayamadim o yuzden alternatif yol
+echo "[ioncube]" >> /etc/php.ini
+echo "zend_extension=/usr/local/ioncube/ioncube_loader_lin_7.3.so" >> /etc/php.ini
+echo "zend_extension_ts=/usr/local/ioncube/ioncube_loader_lin_7.3_ts.so" >> /etc/php.ini
   service php-fpm restart
   echo -e "[${green}DONE${NC}]\n"
