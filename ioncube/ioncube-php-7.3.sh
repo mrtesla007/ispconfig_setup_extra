3a5c19ba343e04168d61ad0697ab79f7fa39af57
BigLorD <FuryTr@yandex.com>
2018-12-31 22:18:10 +0300

    Update ioncube-php-7.3.sh

--- ioncube/ioncube-php-7.3.sh
+++ ioncube/ioncube-php-7.3.sh
@@ -10,6 +10,7 @@
 #sed -i "s%;zend_extension_ts=%zend_extension_ts=/usr/local/ioncube/ioncube_loader_lin_7.3_ts.so" /etc/php.ini
 
   #normalde ustteki komutlar ile calismak lazim ama tam kavrayamadim o yuzden alternatif yol
+echo "
 echo "[ioncube]" >> /etc/php.ini
 echo "zend_extension=/usr/local/ioncube/ioncube_loader_lin_7.3.so" >> /etc/php.ini
 echo "zend_extension_ts=/usr/local/ioncube/ioncube_loader_lin_7.3_ts.so" >> /etc/php.ini
