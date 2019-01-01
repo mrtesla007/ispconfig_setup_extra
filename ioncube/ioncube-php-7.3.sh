ea6cfb4a31510783ec2db6387adc89baa2418ffe
BigLorD <FuryTr@yandex.com>
2019-01-01 12:07:21 +0300

    Update ioncube-php-7.3.sh

--- ioncube/ioncube-php-7.3.sh
+++ ioncube/ioncube-php-7.3.sh
@@ -1,14 +1,12 @@
   echo -n "Installing Ioncube Loader (PHP 7)... "
+  mkdir /usr/local/ioncube
   cd /tmp
   wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
   tar -xzvf ioncube_loaders_lin_x86-64.tar.gz
-  
-  mv /tmp/ioncube /usr/local/ioncube
-  
-#sed -i "s/;zend_extension=/zend_extension=/usr/local/ioncube/ioncube_loader_lin_7.3.so" /etc/php.ini
-#sed -i "s%;zend_extension_ts=%zend_extension_ts=/usr/local/ioncube/ioncube_loader_lin_7.3_ts.so" /etc/php.ini
-
-  #normalde ustteki komutlar ile calismak lazim ama tam kavrayamadim o yuzden alternatif yol
+  # ioncube klasorunu /tmp den usr/local klasorune tasiyoruz 
+  mv /tmp/ioncube -t  /usr/local
+ 
+  #Php.ini dosyamıza asagidaki kodlari ekliyoruz
 echo "
 echo "[ioncube]" >> /etc/php.ini
 echo "zend_extension=/usr/local/ioncube/ioncube_loader_lin_7.3.so" >> /etc/php.ini
