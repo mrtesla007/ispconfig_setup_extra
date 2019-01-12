494fcd3b0e099ff2574eae11c836b2be2e258074
BigLorD <FuryTr@yandex.com>
2019-01-12 14:25:50 +0300

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -6,3 +6,8 @@ baseurl=http://nginx.org/packages/centos/7/$basearch/
 gpgcheck=1
 enabled=1
 gpgkey=https://nginx.org/keys/nginx_signing.key
+
+
+yum update
+yum install nginx
+yum install nginx-all-modules nginx-mod-http-geoip nginx-mod-http-image-filter nginx-mod-http-perl nginx-mod-http-xslt-filter nginx-mod-mail nginx-mod-stream
