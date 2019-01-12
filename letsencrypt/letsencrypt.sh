9a3e8a91084cf2415d76bece14fdbeda879459f0
BigLorD <FuryTr@yandex.com>
2019-01-12 16:33:34 +0300

    Update letsencrypt.sh

--- letsencrypt/letsencrypt.sh
+++ letsencrypt/letsencrypt.sh
@@ -1,1 +1,7 @@
+#https://www.techbrown.com/configure-lets-encrypt-ssl-apache-centos-7/
+#https://www.hostinger.com/tutorials/vps/how-to-install-lets-encrypt-ssl-apache-centos7
 
+cd /opt
+git clone https://github.com/letsencrypt/letsencrypt
+cd letsencrypt/
+./letsencrypt-auto --apache -d test.com --verbose
