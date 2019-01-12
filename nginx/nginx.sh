8136185d7e5eafdb8c5c472d61eccc6d23956a0e
BigLorD <FuryTr@yandex.com>
2019-01-12 15:17:01 +0300

    Update nginx.sh

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,6 +1,8 @@
 #https://nginx.org/en/linux_packages.html
-wget https://raw.githubusercontent.com/b1glord/ispconfig_setup_extra/master/nginx/nginx.repo /etc/yum.repos.d/nginx.repo
+cd /etc/yum.repos.d/
+wget https://raw.githubusercontent.com/b1glord/ispconfig_setup_extra/master/nginx/nginx.repo
 
+cd
 yum update
 yum install nginx
 yum install nginx-all-modules nginx-mod-http-geoip nginx-mod-http-image-filter nginx-mod-http-perl nginx-mod-http-xslt-filter nginx-mod-mail nginx-mod-stream
